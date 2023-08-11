998d31c88f22094bcd4ef1067356cb5030a74017
CaioTakabatake <caio.soldera.taka@gmail.com>
2023-08-11 10:47:26 -0300

    change from todos to todo

--- todolist/src/main/java/br/com/todolist/TodoController.java
+++ todolist/src/main/java/br/com/todolist/TodoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TodoController {
     @GetMapping("/api/todos")
-    public Todo getTodos() {
+    public Todo getTodo() {
         Todo todo = new Todo("Sample Task", "description");
 
         System.out.println(todo);
